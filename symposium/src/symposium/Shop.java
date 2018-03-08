84f2e1ab0f2d2c72c10261a820f7e9808c4c4e8f
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-03-08 15:25:04 -0500

    asdf

--- symposium/src/symposium/Shop.java
+++ symposium/src/symposium/Shop.java
@@ -3,58 +3,136 @@ package symposium;
 import java.util.ArrayList;
 import java.util.List;
 
+import guiTeacher.components.Action;
 import guiTeacher.components.AnimatedComponent;
+import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 import javafx.scene.paint.Color;
 
-public class Shop extends FullFunctionScreen{
+public class Shop extends FullFunctionScreen {
 
-	private TextArea welcomeText; 
+	private Button ONE;
+	private Button TWO;
+	private Graphic inventory;
+	private TextArea welcomeText;
 	private AnimatedComponent armor;
 	public ArrayList<Weapon> weapons;
+	public ArrayList<Weapon> uinventory;
+	public int stock;
+	public int i;
 
-	
 	public Shop(int width, int height) {
-		super(width,height);
+		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		//System.out.println("What would like to do buy we have "+Hero.Weapon.getname());
-	}
-
-	
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		
-		weapons = new ArrayList<Weapon>();
 
-		weapons.add(MainGame.axe);
+		stock = 1;
+
+		weapons = new ArrayList<Weapon>();
+		uinventory = new ArrayList<Weapon>();
+
+		weapons.add(MinuteQuestButBetter.axe);
+		weapons.add(MinuteQuestButBetter.shotgun);
+		weapons.add(MinuteQuestButBetter.spear);
+		weapons.add(MinuteQuestButBetter.stick);
+
 		
-		welcomeText = new TextArea(0,700,800,100,"What weapons would you like to purchase");
+		
+
+		welcomeText = new TextArea(5, 700, 800, 100, "Welcome to armory! We sell the finest weapons. "+"PRO TIP: Click on the weapon you would like to buy.");
 		viewObjects.add(welcomeText);
-		
+
 		armor = new AnimatedComponent(0, 0, 598, 59);
 		viewObjects.add(armor);
 
+		// armor.addSequence("symposium/armor1.png", 200, 0, 0, 597, 58, 10);
+		// Thread asdf = new Thread(armor);
+		// asdf.start();
+
+		Graphic firstitem = new Graphic(100, 100, 60, 60, weapons.get(0).img());
 		
-		//armor.addSequence("symposium/armor1.png", 200, 0, 0, 597, 58, 10);
-		//Thread asdf = new Thread(armor);
-		//asdf.start();
+		Graphic seconditem = new Graphic(100, 200, 60, 60, weapons.get(1).img());
+		
+		Graphic thirditem = new Graphic(100, 300, 60, 60, weapons.get(2).img());
+		
+		Graphic fourthitem = new Graphic(100, 400, 60, 60, weapons.get(3).img());
+		
+		viewObjects.add(firstitem);
+		viewObjects.add(seconditem);
+		viewObjects.add(thirditem);
+		viewObjects.add(fourthitem);
+
 		
 
-		Graphic armor = new Graphic(0, 0, getWidth(), getHeight(), "symposium/armor1.png");
-		viewObjects.add(armor);
-		
-		
-		
-		Graphic axe = new Graphic(0, 0, 100, 100,weapons.get(0).img());
+		ONE = new Button(100, 100, 60, 60, "", new Action() {
 
-		viewObjects.add(axe);
+			@Override
+			public void act() {
+
+				if (stock == 1)
+				{
+					displayInventory();
+					
+
+					uinventory.add(weapons.get(0));
+					displayInventory();
+					//Graphic inventory = new Graphic(700, 700, 60, 60, uinventory.get(0).img());
+					viewObjects.add(inventory);
+					stock = 0;
+
+					ONE.setEnabled(false);
+					ONE.setVisible(false);
+
+				} 
+
+			}
+		});
+
+		viewObjects.add(ONE);
+		
+		
+		TWO = new Button(100, 200, 60, 60, "", new Action() {
+
+			@Override
+			public void act() {
+
+				if (stock == 1)
+				{
+					
+					
+					System.out.print("Add to inventory.");
+
+					uinventory.add(weapons.get(1));
+					displayInventory();
+					//Graphic inventory = new Graphic(700, 700, 60, 60, uinventory.get(0).img());
+					viewObjects.add(inventory);
+					stock = 0;
+
+					TWO.setEnabled(false);
+					TWO.setVisible(false);
+
+				} 
+
+			}
+		});
+
+		viewObjects.add(TWO);
 	}
+		
+	
+	public void displayInventory()
+	{
+		for(i=0; i<uinventory.size(); i++)
+		{
+			inventory = new Graphic(700, i*100, 60, 60, uinventory.get(i).img());
 
+		}
+	}
+		
 }
